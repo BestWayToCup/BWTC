a81af18302c1a9e47575522ef68254212277dcf8
BestWayToCup <36342914+BestWayToCup@users.noreply.github.com>
2018-02-11 16:06:46 +0300

    New

--- server.js
+++ server.js
@@ -1,29 +1,41 @@
-var express = require('express');
-var app = express();
-var bodyParser = require('body-parser');
-//var db = require('./lib/db');
+// Get dependencies
+const express = require('express');
+const path = require('path');
+const http = require('http');
+const bodyParser = require('body-parser');
 
-//app.use(function(req, res, next) {
-//    res.header("Access-Control-Allow-Origin", "*");
-//    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-//    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//    next();
-//});
+// Get our API routes
+const api = require('./server/routes/api');
+
+const app = express();
+
+// Parsers for POST data
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.all('/', function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
+// Point static path to dist
+app.use(express.static(path.join(__dirname, 'dist')));
+
+// Set our api routes
+app.use('/api', api);
+
+// Catch all other routes and return the index file
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'dist/index.html'));
 });
 
-//app.use(require('./routes/api'));
+/**
+ * Get port from environment and store in Express.
+ */
+const port = process.env.PORT || '8080';
+app.set('port', port);
 
-app.use('*', require('./dist/index.html'));
+/**
+ * Create HTTP server.
+ */
+const server = http.createServer(app);
 
-var port = process.env.PORT || 8080;
-
-app.listen(port, () => {
-    console.log("Listening to port " + port);
-});
+/**
+ * Listen on provided port, on all network interfaces.
+ */
+server.listen(port, () => console.log(`API running on localhost:${port}`));
